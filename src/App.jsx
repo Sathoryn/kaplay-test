1ef43717ab5bcc69ea1cadb052ec740da0582e6a
Dan Moffat <danmoffat@gmail.com>
2025-11-20 09:55:15 +0000

    Add simple counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import initGame from './game/initGame';
 import './App.css';
 
 export function App() {
+  const [count, setCount] = useState(0);
   const game = useRef(null);
 
   useEffect(() => {
@@ -11,7 +12,17 @@ export function App() {
     }
   }, []);
 
-  return <canvas ref={game}></canvas>;
+  const onClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={onClick}>Click</button>
+      <canvas ref={game}></canvas>;
+    </div>
+  );
 }
 
 export default App;
